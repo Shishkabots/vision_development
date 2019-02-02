552628f0aa73b7a6bae1a96b9dd8f69d43dc2e20
IsitaT03 <36215948+IsitaT03@users.noreply.github.com>
2019-02-01 16:49:30 -0800

    Update centertest.py

--- centertest.py
+++ centertest.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 img = cv.imread('star.jpg',0)
 ret,thresh = cv.threshold(img,127,255,0)
-im2, contours, hierarchy = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 cnt = contours[0]
 M = cv.moments(cnt)
 print(M)
