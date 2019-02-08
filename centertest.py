0cc2199c6344a117d0251a1ab4bfb3758496244e
ArjunGupte1 <agupte1011@gmail.com>
2019-02-07 20:02:35 -0800

    after debugging

--- centertest.py
+++ centertest.py
@@ -2,11 +2,14 @@ import numpy as np
 import cv2 as cv
 img = cv.imread('star.jpg',0)
 ret,thresh = cv.threshold(img,127,255,0)
-im2, contours, hierarchy = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
 cnt = contours[0]
 M = cv.moments(cnt)
 print(M)
-cx = int(M['m10']/M['m00'])
-cy = int(M['m01']/M['m00'])
-print("CX: ", cx)
-print("CY: ", cy)
+if M["m00"] != 0:
+ cx = int(M["m10"] / M["m00"])
+ cy = int(M["m01"] / M["m00"])
+ print("CX: ", cx)
+ print("CY: ", cy)
+else:
+ cX, cY = 0, 0
